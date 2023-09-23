a46eeac315471c5ffe51f50a54aad31c2246b1cc
misa256 <riror6ohar9u312@gmail.com>
2023-09-23 13:59:27 +0900

    いらないコメント消去

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -4,7 +4,6 @@ import com.luv2code.springbootlibrary.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
